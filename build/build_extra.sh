0ad6be9f63c6b1b569db39ed6e7b8e7210166510
qianbozhang <2625465124@qq.com>
2022-08-06 19:25:48 +0800

    ffmpeg v4.2.2

--- build/build_extra.sh
+++ build/build_extra.sh
@@ -23,20 +23,20 @@ else
 fi;
 
 #ffmpeg
-mkdir -p $BUILD_TEMP/FFmpeg
-cd $BUILD_TEMP/FFmpeg
+mkdir -p $BUILD_TEMP/ffmpeg-4.2.2
+cd $BUILD_TEMP/ffmpeg-4.2.2
 if [ ! -f "build_ok" ]; then
-    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>> build FFmpeg >>>>>>>>>>>>>>>>>>>>>>>";
-    CC=$CC $EXTRA_DIR/FFmpeg/configure --prefix=$INSTALL_DIR \
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>> build ffmpeg-4.2.2 >>>>>>>>>>>>>>>>>>>>>>>";
+    CC=$CC $EXTRA_DIR/ffmpeg-4.2.2/configure --prefix=$INSTALL_DIR \
         --enable-shared
     make -j 8 && make install
     if [ $? -eq 0 ]; then
         touch build_ok;
-        echo "FFmpeg compiled successfully, please delete \"build_ok\" file in $BUILD_TEMP/FFmpeg if you wanted rebuild.";
+        echo "ffmpeg-4.2.2 compiled successfully, please delete \"build_ok\" file in $BUILD_TEMP/ffmpeg-4.2.2 if you wanted rebuild.";
     else
-        echo "FFmpeg compiled fail, please fixed error and retry.";
+        echo "ffmpeg-4.2.2 compiled fail, please fixed error and retry.";
         exit 0;
     fi;
 else
-    echo "FFmpeg has been compiled.";
+    echo "ffmpeg-4.2.2 has been compiled.";
 fi;
